a018faef2e159e57afff95824df9d50c6d34828d
irlartboy <ollie_king99@outlook.com>
2019-05-01 10:26:48 +1000

    update

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float runSpeed = 8f;
+    public float walkSpeed = 6f;
+    public float gravity = 15f;
+    public float crouchSpeed = 4f;
+    public float jumpHeight = 8f;
+    public float groundRayDistance = 1.1f;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
